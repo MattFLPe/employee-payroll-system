060172856be517dad194f594ca5c29664d84c509
Mateus Felipe <brasil.mateus748@gmail.com>
2024-07-09 23:15:45 -0300

    Create method to calculate the gross pay

--- src/Employee.java
+++ src/Employee.java
@@ -1,5 +1,9 @@
 import java.util.*;
-public class Employee {
+public class Employee extends Payroll {
+    public float regularHours;
+    private float overtimeHours;
+    private float bonus;
+    private float deductions;
     String name;
     int ID;
     float salary;
@@ -10,20 +14,41 @@ public class Employee {
         this.name = s.nextLine();
         System.out.println("Enter employee's ID: ");
         this.ID = s.nextInt();
-        System.out.println("Enter employee's salary: ");
+        System.out.println("Enter employee's hourly salary: ");
         this.salary = s.nextFloat();
-
+        System.out.println("Enter employee's regular hours worked: ");
+        this.regularHours = s.nextFloat();
+        System.out.println("Enter employee's overtime hours worked: ");
+        this.overtimeHours = s.nextFloat();
+        System.out.println("Enter employee's bonus: ");
+        this.bonus = s.nextFloat();
+        System.out.println("Enter employee's deductions: ");
+        this.deductions = s.nextFloat();
 
         System.out.println("Employee's name: " + name + " " +
                             "Employee's ID: " + ID + " " +
-                            "Employee's salary: " + salary);
+                            "Employee's hourly salary: " + salary + " " +
+                            "Regular hours worked: " + regularHours + " " +
+                            "Overtime hours worked: " + overtimeHours + " " +
+                            "Bonus: " + bonus + " " +
+                            "Deductions: " + deductions);
+                            }
+    public float calculateGrossPay() {
+        float overtimeRate = 1.5f;
+        float regularPay = regularHours * salary;
+        float overtimePay = overtimeHours * salary * overtimeRate;
+        return regularPay + overtimePay + bonus;
     }
 
     public String[] storeEmployeeData() {
-        String[] employeeData = new String[3];
+        String[] employeeData = new String[7];
         employeeData[0] = this.name;
         employeeData[1] = Integer.toString(this.ID);
         employeeData[2] = Float.toString(this.salary);
+        employeeData[3] = Float.toString(this.regularHours);
+        employeeData[4] = Float.toString(this.overtimeHours);
+        employeeData[5] = Float.toString(this.bonus);
+        employeeData[6] = Float.toString(this.deductions);
         return employeeData;
     }
 
